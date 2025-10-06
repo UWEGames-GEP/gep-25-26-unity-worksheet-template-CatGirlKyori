4895e2797e81eda36742e6818244ed1e8afc1826
Harrison <Harrison2.Roberts@live.uwe.ac.uk>
2025-10-06 12:18:40 +0100

    Week 1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameState 
+    {
+        PLAY, PAUSE
+    }
+
+    public GameState state;
+    public bool hasChangedState;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        state = GameState.PLAY;
+        hasChangedState = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == GameState.PLAY)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                state = GameState.PAUSE;
+                hasChangedState = true;
+            }
+        }
+
+        else if (state == GameState.PAUSE)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                state = GameState.PLAY;
+                hasChangedState = true;
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (hasChangedState)
+        {
+            hasChangedState = false;
+
+            if (state == GameState.PLAY)
+            {
+                Time.timeScale = 1.0f;
+            }
+
+            else if (state == GameState.PAUSE)
+            {
+                Time.timeScale = 0.0f;
+            }
+        }
+    }
+}
